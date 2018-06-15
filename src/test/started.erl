a3820d153add56fa51b131bb5ed701ae623b577b
Gelomen <kai664792415@qq.com>
2018-06-15 13:37:52 +0800

    update

--- src/test/started.erl
+++ src/test/started.erl
@@ -385,6 +385,73 @@ fold(Fun, Start, [H | T]) -> fold(Fun, Fun(H, Start), T).
 
 
 
+% ============================== 处理异常 ==============================
+
+% 编写这个函数，用不同的异常实验
+throws(F) ->
+  try F() of
+      _ -> ok
+  catch
+      Throw -> {throw, caught, Throw}
+  end.
+
+% 其他类型
+errors(F) ->
+  try F() of
+    _ -> ok
+  catch
+    error:Error -> {error, caught, Error}
+  end.
+
+exits(F) ->
+  try F() of
+    _ -> ok
+  catch
+    exit:Exit -> {exit, caught, Exit}
+  end.
+
+% 在单个 try ... catch 捕获所有异常
+% 定义一个能产生所有类型异常的函数
+sword(1) -> throw(slice);
+sword(2) -> erlang:error(cut_arm);
+sword(3) -> exit(cut_leg);
+sword(4) -> throw(punch);
+sword(5) -> exit(cross_bridge).
+
+% is_function/2 是一个BIF函数，确保 Attack 是一个参数为 0 的函数
+black_knight(Attack) when is_function(Attack, 0) ->
+  try Attack() of
+    _ -> "None shall pass"
+  catch
+    throw: slice -> "It is but a scratch.";
+    error: cut_arm -> "I've had worse.";
+    exit: cut_leg -> "Come on you pussy!";
+    _: _ -> "Just a flesh wound."
+  end.
+
+talk() -> "bla bla bla".
+
+
+% ==== try 和 of 之间可以有多个表达式
+
+whoa() ->
+  try
+      talk(),
+      _Knight = "None shall pass!",
+      _Double = [ N * 2 || N <- lists:seq(1, 100)],
+      throw(up),
+      _WillReturnThis = tequila
+% 当使用了多个表达式时，我们有时就可能不太会在意返回值了，所以去掉 of
+% of
+%     tequila -> "Hey, this worked!"
+  catch
+      Exception: Reason -> {caught, Exception, Reason}
+  end.
+
+
+
+
+
 
 
 
