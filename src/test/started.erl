93a85ed823b45450eb04dc59c4ba3aff0e4e0ea3
Gelomen <kai664792415@qq.com>
2018-06-17 14:06:37 +0800

    update

--- src/test/started.erl
+++ src/test/started.erl
@@ -534,7 +534,27 @@ rpn("prod", [H | []]) -> [H];
 rpn("prod", [N1, N2 | T]) -> rpn("prod", [N1 * N2 | T]);
 rpn(X, Start) -> [read(X) | Start].
 
+% 代码测试
 
+rpn_test() ->
+  5 = rpn("2 3 +"),
+  87 = rpn("90 3 -"),
+  -4 = rpn("10 4 3 + 2 * -"),
+  -2.0 = rpn("10 4 3 + 2 * - 2 /"),
+  ok = try
+      rpn("90 34 12 33 55 66 + * - +")
+  catch
+      error: {badmatch, [_|_]} -> ok
+  end,
+  4037 = rpn("90 34 12 33 55 66 + * - + -"),
+  8.0 = rpn("2 3 ^"),
+  true = math:sqrt(2) == rpn("2 0.5 ^"),
+  true = math:log(2.7) == rpn("2.7 ln"),
+  true = math:log10(2.7) == rpn("2.7 log10"),
+  50 = rpn("10 10 10 20 sum"),
+  10.0 = rpn("10 10 10 20 sum 5 /"),
+  1000.0 = rpn("10 10 20 0.5 prod"),
+  ok.
 
 
 
