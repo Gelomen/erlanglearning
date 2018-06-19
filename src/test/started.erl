f3b2bd7949639d6c82634d1a4ef0f172e30988cc
Gelomen <kai664792415@qq.com>
2018-06-19 23:02:11 +0800

    update

--- src/test/started.erl
+++ src/test/started.erl
@@ -670,7 +670,7 @@ end.
 dolphin3() ->
   receive
     {From, do_a_flip} ->
-      From ! "How about no? ~n",
+      From ! "How about no?",
       dolphin3();
     {From, fish} ->
       From ! "So long and thanks for all the fish!";
